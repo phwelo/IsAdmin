4647bddbaa0fa18e3bc708ab6880e61ee50d6d7d
Phwelo <phwelo@gmail.com>
2015-01-05 15:19:10 -0500

    Added Date field
    added date field to SQL server as well as date field to powershell
    script

--- IsAdmin.ps1
+++ IsAdmin.ps1
@@ -1,18 +1,19 @@
 #Connect to the SQL database ScriptOutput Table AdminQuery
 $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+
 #Write-only access via scriptuser sql account
 $SqlConnection.ConnectionString = "Server=as2.levelone.local;Database=ScriptOutput;User Id=scriptuser;Password=scr1pt3dPW;"
 $SqlConnection.Open()
 $Command = New-Object System.Data.SQLClient.SQLCommand
 $Command.Connection = $SqlConnection
-
+$CurDate = Get-Date
 
 #Get variables we'll need to reuse
 $username = (gc env:username)
 $computername = (gc env:computername)
 $ipaddresses = [System.Net.Dns]::GetHostAddresses($computername)|select-object IPAddressToString -expandproperty IPAddressToString
 
-#Check if there is a record
+#Check if there is a record already
 $Query = "SELECT * FROM [ScriptOutput].[dbo].[AdminQuery] WHERE [CompName] LIKE '$computername'"
 $Command2=new-object system.Data.SqlClient.SqlCommand($Query,$SqlConnection)
 $Dataset=New-Object system.Data.DataSet
@@ -35,11 +36,13 @@ $Command.CommandText = "UPDATE [ScriptOutput].[dbo].[AdminQuery] SET IsAdmin = '
 $Command.ExecuteNonQuery() | out-null
 $Command.CommandText = "UPDATE [ScriptOutput].[dbo].[AdminQuery] SET IsMB = '$ismb' WHERE CompName = '$computername'"
 $Command.ExecuteNonQuery() | out-null
+$Command.CommandText = "UPDATE [ScriptOutput].[dbo].[AdminQuery] SET Updated = '$curdate' WHERE CompName = '$computername'"
+$Command.ExecuteNonQuery() | out-null
 }
 #If it's a new entry, Insert All Values
 ELSE
 {
 #Actually execute the INSERT to SQL
-$Command.CommandText = "INSERT INTO AdminQuery (CompName,UserName,IP,IsAdmin,IsMB) VALUES ('$computername','$username','$ipaddresses','$isadmin','$ismb')"
+$Command.CommandText = "INSERT INTO AdminQuery (CompName,UserName,IP,IsAdmin,IsMB,Updated) VALUES ('$computername','$username','$ipaddresses','$isadmin','$ismb','$curdate')"
 $Command.ExecuteNonQuery() | out-null
 }
